f66e77fbaf4e13994a01d364eac53cd0b56edf6a
Joseph Toups <josephtoups95@gmail.com>
2021-07-11 13:03:47 -0700

    models
    debugging

--- models/index.js
+++ models/index.js
@@ -1,14 +1,24 @@
-const User = require('./user');
-const Post = require('./blogPosts');
-
-
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
 
-module.exports = {User, Post};
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);;
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({helpers});
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'secret',
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -26,16 +22,18 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
 
+app.use(require('./controllers/'));
 
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
